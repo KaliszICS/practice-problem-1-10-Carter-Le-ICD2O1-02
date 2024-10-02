2237a0bc52b69105c898661036d64918c9818c79
Carter-Le-ICD2O1-02 <carter.le@student.tdsb.on.ca>
2024-10-02 17:20:40 +0000

    import mat

--- PP1_10.py
+++ PP1_10.py
@@ -1,31 +1,41 @@
-
+import math
 
 def q1(): 
   #Write Assignment code here
+  io1uo = float(input("Input a number: "))
+  print(math.sqrt(io1uo))
 
 
 def q2(): 
   #Write Assignment code here
+  unj = int(input("Input a number: "))
+  print(math.isqrt(unj))
 
 
 def q3(): 
   #Write Assignment code here
-
+  rou = float(input("Input a number: "))
+  print(math.floor(rou))
 
 def q4(): 
   #Write Assignment code here
-
+  cei = float(input("Input a number: "))
+  print(math.ceil(cei))
 
 def q5(): 
   #Write Assignment code here
-
+  num1 = float(input("Input a number: "))
+  num2 = float(input("Input another number: "))
+  num33 = num1 * num2
+  num33 = num33 / 2
+  print(math.floor(num33))
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
